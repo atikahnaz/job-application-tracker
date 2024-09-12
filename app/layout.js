e7d16da676fcbeb393d09cb0bd635d6cda535863
Atikah <atikahnazir6@gmail.com>
2024-09-12 16:06:15 +0800

    feat: import AppProvider from @toolpad/core for app/layout

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import localFont from "next/font/local";
+import { AppProvider } from "@toolpad/core";
 import "./globals.css";
 
 const geistSans = localFont({
@@ -23,7 +24,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <AppProvider>{children}</AppProvider>
       </body>
     </html>
   );
